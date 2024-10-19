5685b983fad48e36512157b3ba890b417341282e
Andrea Sdringola <andrea@ufficio>
2024-10-19 16:05:28 +0200

    User_Story_1 Corretta

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,20 +4,41 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
+use Database\Seeders\DatabaseSeeder;
 
 class DatabaseSeeder extends Seeder
 {
+    public $categories = [
+        'elettronica',
+        'abbigliamento',
+        'salute e bellezza',
+        'casa e giardinaggio',
+        'giocattoli',
+        'sport',
+        'animali domestici',
+        'libri e riviste',
+        'accessori',
+        'motori'
+
+    ];
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
+        
+        foreach ($this->categories as $category) {
+            Category::create([
+                'name' => $category
+            ]);
+        }
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        //User::factory()->create([
+            //'name' => 'Test User',
+           // 'email' => 'test@example.com',
+        //]);
     }
 }
